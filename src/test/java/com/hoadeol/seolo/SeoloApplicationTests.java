5bb31d965e1fa739d38d41b326691692c197bedb
mi-hwa <43jmih@naver.com>
2024-01-05 14:10:41 +0900

    프로젝트 생성

---
+++ src/test/java/com/hoadeol/seolo/SeoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoadeol.seolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoloApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
